1e1d845d1612bb9d341f07b55600ac3ff46a1e7f
Ivanus Kuzminus <kuzminivan2004@gmail.com>
2018-07-03 17:16:32 +0000

    Add files via upload
    Loaded the preliminary code for the discord bot.

---
+++ OurLittleBot.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+
+
+Client = discord.Client()
+client = commands.Bot(command_prefix = "?")
+
+
+@client.event 
+async def on_ready():
+    print("Bot is online and connected to Discord")
+    
+@client.command()
+async def ping():
+    await client.say("Pong!")
+    
+@client.command()
+async def echo(*args):
+    output = ""
+    for word in args:
+        output += word
+        output += " "
+    await client.say(output)
+
+client.run("NDYyMzIxNTQ4OTU1NDg0MTYw.DhgZCA.CHBQy6CYpzTJ2HSdoc9NOio-We8")
