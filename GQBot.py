5c70c8a781bbf2a60e66ce37c50c2a1e6ab358c1
Ivanus Kuzminus <kuzminivan2004@gmail.com>
2019-08-06 17:17:06 +0100

    Update GQBot.py
    New update! Finally!
    Some things still need to be fixed in order to work with discord.py 1.0

--- GQBot.py
+++ GQBot.py
@@ -4,67 +4,78 @@ from discord.ext import commands
 import asyncio
 import time
 
-
-Client = discord.Client()
 client = commands.Bot(command_prefix = "$")
-
 
 @client.event 
 async def on_ready():
-    await client.change_presence(game=discord.Game(name="$commands"))
     print("GQBot is online! Hail the Fathers!")
     
 @client.command()
-async def ping():
-    await client.say("Pong!")
+async def ping(ctx):
+    await ctx.send("Pong!")
 
 @client.command()
-async def spam100():
+async def spam100(ctx):
     n = 1
     while 1 < 101:
-        await client.say("@everyone bow down to the GQEmpire")
+        await ctx.send("@everyone bow down to the GQEmpire")
         n += 1
 
-@client.command()
-async def romatime():
-    await client.say("http://romatime.gq")
-    await client.say("The most recent episode is: " + "https://www.youtube.com/watch?v=pUcxS8Cnyfg")
+"""
+Still needs to be migrated
+@commands.Cog.listener()
+async def on_message(ctx, message):
+    if message.content == "Greetings, GQBot!":
+        await ctx.send_message(message.channel, "Hail the Fathers!")
+    elif message.content == "greetings, GQBot!":
+        await ctx.send_message(message.channel, "Hail the Fathers!")
+    elif message.content == "greetings GQBot!":
+        await ctx.send_message(message.channel, "Hail the Fathers!")
+    await ctx.process_commands(message)
+"""
 
 @client.command()
-async def aquaesulis():
-    await client.say("http://aquaesulis.gq")
+async def romatime(ctx):
+    await ctx.send("http://romatime.gq")
+    await ctx.send("The most recent episode is: " + "https://www.youtube.com/watch?v=pUcxS8Cnyfg")
 
 @client.command()
-async def tiffinbbc():
-    await client.say("http://tiffinBBC.gq")
-    await client.say("WARNING! Formatting might look relly weird on big screens!")
+async def aquaesulis(ctx):
+    await ctx.send("http://aquaesulis.gq")
+
+@client.command()
+async def tiffinbbc(ctx):
+    await ctx.send("http://tiffinBBC.gq")
+    await ctx.send("WARNING! Formatting might look really weird on big screens!")
     
 @client.command()
-async def github():
-    await client.say("https://github.com/ivan-kuz/GQBot/")
-    await client.say("This is the github repo for the GQBot. If you're an avid coder, feel free to contribute to this wonderful bot!")
+async def github(ctx):
+    await ctx.send("https://github.com/ivan-kuz/GQBot/")
+    await ctx.send("This is the github repo for the GQBot. If you're an avid coder, feel free to contribute to this wonderful bot!")
     
 @client.command()
-async def commands():
-    await client.say("https://github.com/ivan-kuz/GQBot/wiki")
-    await client.say("This is the list of all of the commands of the GQBot. We hope it's up to date: if not, just let us know!")
+async def wiki(ctx):
+    await ctx.send("https://github.com/ivan-kuz/GQBot/wiki")
+    await ctx.send("This is the link to the wiki for the GQBot. If you need some help - you will definitely find it on there!")
 
 @client.command()
-async def quests():
-    await client.say("Press 'Run' to play your first quest!")
-    await client.say("http://gq-quests.atwebpages.com")
+async def quests(ctx):
+    await ctx.send("Press 'Run' to play your first quest!")
+    await ctx.send("http://gq-quests.atwebpages.com")
 
+"""
+Still needs to be migrated
 @client.event
-async def on_message(message):
+async def on_message(ctx, message):
     if message.content == "calm":
-        await client.send_message(message.channel, "Do you know what else is calm?! The GQBot!")
+        await ctx.send_message(message.channel, "Do you know what else is calm?! The GQBot!")
     elif message.content == "calm.":
-        await client.send_message(message.channel, "Do you know what else is calm?! The GQBot!")
+        await ctx.send_message(message.channel, "Do you know what else is calm?! The GQBot!")
     await client.process_commands(message)
-        
+"""
 @client.command()
-async def gqempire():
-    await cleint.say("http://GQEmpire.gq")
-    await client.say("This is your main site! Feel free to make it your homepage :)")
+async def gqempire(ctx):
+    await ctx.send("http://GQEmpire.gq")
+    await ctx.send("This is your main site! Feel free to make it your homepage :)")
 
-client.run("token")
+client.run("im-not-telling-you-the-token")
