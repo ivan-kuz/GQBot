267ce26f2829b5f556d434e58263c28d84fecbb6
eutropius225 <11538216+eutropius225@users.noreply.github.com>
2019-12-17 22:22:46 +0000

    Update botconstants.py

--- botconstants.py
+++ botconstants.py
@@ -2,6 +2,15 @@ import json
 
 config = json.load(open("config.json", "r"))
 
+"""
+config.json needs to be filled in:
+{
+  "TOKEN": "<token>",
+  "PREFIX_RAW": "<prefix>",
+  "PREFIX_ARRAY": [<prefix>, <prefix_alias_1>, <prefix_alias_2>, ...]
+}
+"""
+
 TOKEN = config["TOKEN"]
 PREFIX_RAW = config["PREFIX_RAW"]
 PREFIX_LEN = len(PREFIX_RAW)
